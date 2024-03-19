465bba3ff7ccccd9aad72ff1b1d0c4f866980660
alexismrosales <aleisjorge91@gmail.com>
2024-03-19 14:39:18 -0600

    set backend project settings

---
+++ url_shortener-backend/src/test/java/com/alxmr/url_shortenerbackend/UrlShortenerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alxmr.url_shortenerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
