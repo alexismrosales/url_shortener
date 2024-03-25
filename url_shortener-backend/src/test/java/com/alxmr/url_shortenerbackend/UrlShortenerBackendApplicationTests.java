3dd1b9497b885a5efe7f9775d7227cf49e8929da
rarksz <aleisjorge91@gmail.com>
2024-03-25 02:30:31 -0600

    Created tests

--- url_shortener-backend/src/test/java/com/alxmr/url_shortenerbackend/UrlShortenerBackendApplicationTests.java
+++ url_shortener-backend/src/test/java/com/alxmr/url_shortenerbackend/UrlShortenerBackendApplicationTests.java
@@ -1,13 +1,16 @@
 package com.alxmr.url_shortenerbackend;
 
+import com.alxmr.url_shortenerbackend.service.implementation.logic.ShortenURL;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class UrlShortenerBackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testURLHashing() {
 	}
 
 }
