5efcbb367bd505d490e36391c1e9313e4d80b3a1
rarksz <aleisjorge91@gmail.com>
2024-03-26 19:29:26 -0600

    Updated view

--- url_shortener-frontend/src/components/shortener/index.tsx
+++ url_shortener-frontend/src/components/shortener/index.tsx
@@ -2,9 +2,22 @@ import React from "react";
 
 import { createSURL, checkBackHalf } from "../../api/fetchData";
 
-const createShortURLContainer: React.FC = () => {
+import style from "./styles.module.css"
+
+const ShortURLContainer = () => {
+
   return (
-    <>
-    </>
+    <div className={style.container}>
+
+      <form>
+        <div className={style.mainInput}>
+          <label className={style.subtext}>To short your link paste it below: </label>
+          <br />
+          <input type="url" placeholder="Paste your link here" />
+          <button type="submit" className="rounded-lg">SHORT</button>
+        </div>
+      </form>
+    </div>
   );
 };
+export default ShortURLContainer;
