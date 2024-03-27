858b248a9c96ccafbb2bca2934f20554efa87a6e
rarksz <aleisjorge91@gmail.com>
2024-03-27 00:09:24 -0600

    some changes

--- url_shortener-frontend/src/api/fetchData.tsx
+++ url_shortener-frontend/src/api/fetchData.tsx
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 const baseUrl = apikey;
 
-export const createSURL = (URLSItem: any, back_half: any, needsQR: any) => axios.post(baseUrl + "/" + back_half + "/" + needsQR, URLSItem);
-export const checkBackHalf = (back_half: any) => axios.post(baseUrl + "/" + back_half);
+export const createSURL = (URLSItem: any, back_half: string, needsQR: boolean) => axios.post(baseUrl + "/" + back_half + "/" + needsQR, URLSItem);
+export const checkBackHalf = (back_half: string) => axios.post(baseUrl + "/" + back_half);
