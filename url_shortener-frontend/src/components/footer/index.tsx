a9ad246c2887af691bb54623d8cc42e32c355050
rarksz <aleisjorge91@gmail.com>
2024-03-26 19:26:29 -0600

    new component added

---
+++ url_shortener-frontend/src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import style from "./styles.module.css"
+
+const Footer = () => {
+  return (
+    <footer className={style.container}>
+      <p className="text-white">Made by Alexis M.Rosales</p>
+    </footer>
+  )
+}
+export default Footer;
